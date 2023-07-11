4f32a6fca2febcef5df9e63c7c74c594f4eea01b
Nile Okomo <okomonile3@gmail.com>
2023-07-11 23:57:09 +0300

    Unstash base_model file

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a base class BaseModel"""
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+    """Defines all the common attributes/methods for other classes
+    """
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """Overwritten str representation"""
+        rep = f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return rep
+
+    def save(self):
+        """Updates self.updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all key/values of the object
+        """
+        dict_rep = self.__dict__.copy()
+        dict_rep['__class__'] = self.__class__.__name__
+        dict_rep['created_at'] = self.created_at.isoformat()
+        dict_rep['updated_at'] = self.updated_at.isoformat()
+
+        return dict_rep
