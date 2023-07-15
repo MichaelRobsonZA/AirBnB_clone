f87c4c946945be32bcda045e460b53986c4af5e5
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-07-15 10:15:38 -0700

    Implemented BaseModel class: Defines the BaseModel class, which serves as the base model for all other classes in the project. It provides common attributes and methods such as id, created_at, updated_at, save(), to_dict(), and __str__(). The class supports object initialization from both positional arguments and keyword arguments.

--- models/base_model.py
+++ models/base_model.py
@@ -15,24 +15,22 @@ class BaseModel:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
         """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs:
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
             for k, v in kwargs.items():
-                if k == "__class__":
-                    continue
                 if k == "created_at" or k == "updated_at":
-                    dt = datetime.fromisoformat(v)
-                    setattr(self, k, dt)
+                    self.__dict__[k] = datetime.strptime(v, tform)
                 else:
-                    setattr(self, k, v)
+                    self.__dict__[k] = v
         else:
             models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.now()
+        """Update updated_at with the current datetime and save to storage."""
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
@@ -40,6 +38,9 @@ class BaseModel:
 
         Includes the key/value pair __class__ representing
         the class name of the object.
+
+        Returns:
+            dict: The dictionary representation of the BaseModel instance.
         """
         rdict = self.__dict__.copy()
         rdict["created_at"] = self.created_at.isoformat()
@@ -48,6 +49,10 @@ class BaseModel:
         return rdict
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
+        """Return the print/str representation of the BaseModel instance.
+
+        Returns:
+            str: The string representation of the BaseModel instance.
+        """
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
