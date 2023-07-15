b94423e511f9ba09dd1e6afb251b46ceaa78e836
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-07-15 10:16:40 -0700

    Implemented FileStorage class: Defines the FileStorage class, which serves as an abstracted storage engine. It handles the serialization and deserialization of objects to and from a JSON file. The class provides methods such as all(), new(), save(), and reload() to manage objects in memory and save/load them from the JSON file.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,12 @@
 """Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -19,7 +25,11 @@ class FileStorage:
         return FileStorage.__objects
 
     def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id"""
+        """Set in __objects obj with key <obj_class_name>.id
+
+        Args:
+            obj (BaseModel): The object to set in __objects.
+        """
         ocname = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
